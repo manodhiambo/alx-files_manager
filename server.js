37802bac8d74103dda23ff8028438b761276b3f6
manodhiambo <manodhiambo@gmail.com>
2022-08-20 05:59:42 +0000

    Fix

--- server.js
+++ server.js
@@ -1,20 +1,6 @@
 import express from 'express';
 import controllerRouting from './routes/index';
 
-/**
- * This project is a summary of back-end concepts:
- * authentication, NodeJS, MongoDB, Redis,
- * pagination and background processing.
- *
- * The objective was to build a simple platform to upload and view files:
- *
- * User authentication via a token
- * List all files
- * Upload a new file
- * Change permission of a file
- * View a file
- * Generate thumbnails for images
- */
 
 const app = express();
 const port = process.env.PORT || 5000;
